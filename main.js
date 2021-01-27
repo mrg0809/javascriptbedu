c4398111c40ed038a2ae4173e89d88876c7a1666
MarceloRodriguez <mrg0809@gmail.com>
2021-01-26 20:15:57 -0600

    tercer ejercicio

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var edad = 18;
+var limite = 20;
+var ciudad = "cdmx";
 
-if(edad=>18){
+if(edad=>limite && ciudad=="cdmx") {
     console.log("Puedes votar")}
    else {
         console.log("no puedes votar.");
