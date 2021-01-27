130a0c13d2ddf422be6564dac92079363d2138a2
MarceloRodriguez <mrg0809@gmail.com>
2021-01-26 20:35:05 -0600

    cuarto ejercicio

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-var edad = 18;
+/*var edad = 18;
 var limite = 20;
 var ciudad = "cdmx";
 
-if(edad=>limite && ciudad=="cdmx") {
+if(edad>=limite && ciudad=="cdmx") {
     console.log("Puedes votar")}
    else {
         console.log("no puedes votar.");
     }
+*/
+let autor = 'Miguel de Cervantes';
+
+switch(autor) {
+    case 'Miguel de Cervantes':
+        console.log('Don Quijote');
+        break;
+    case 'Juan Rulfo':
+        console.log('el gallo de oro');
+        break;
+    case 'Gabriel Garcia Marquez':
+        console.log('Cien anos de soledad');
+        break;
+    default:
+        console.log('No existe')
+        break;
+}
