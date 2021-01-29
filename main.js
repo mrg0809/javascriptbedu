63142d7a0a9d7ae8b2e9e78ba0d123332862fe34
MarceloRodriguez <mrg0809@gmail.com>
2021-01-28 20:02:46 -0600

    ejercicio2

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-function saludar(saludo, name) {
+/*function saludar(saludo, name) {
     alert(saludo + ' usuario ' + name);
 }
-saludar('Hola', 'Pedro');
+saludar('Hola', 'Pedro');*/
+
+/*function perimetro(name, size1, size2, size3, size4, size5) {
+    alert(size1+size2+size3+size4+size5);
+}
+
+perimetro('cuadro',4,4,4,4,0);*/
+
