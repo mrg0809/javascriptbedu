bf223ac4bf2e10807e6316fe263d597e6aea90b3
MarceloRodriguez <mrg0809@gmail.com>
2021-01-28 20:21:26 -0600

    ejercicio3

--- main.js
+++ main.js
@@ -3,9 +3,30 @@
 }
 saludar('Hola', 'Pedro');*/
 
-/*function perimetro(name, size1, size2, size3, size4, size5) {
-    alert(size1+size2+size3+size4+size5);
+var figura=(prompt('Que figura es:'));
+
+function cuadradorect(size1, size2, size3, size4) {
+    alert(size1+size2+size3+size4);
 }
 
-perimetro('cuadro',4,4,4,4,0);*/
+function circulo(radio){
+    alert(2*3.1416*radio);
+}
+
+switch (figura){
+
+    case 'cuadrado':
+        cuadradorect(4,4,4,4);
+        break;
+
+    case 'circulo':
+        circulo(50);
+        break;
+
+
+    default:
+        alert("no tengo esa figura");
+        break;
+
+}
 
