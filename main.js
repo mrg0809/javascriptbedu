afd1349c14dcdcd659b740fba3dfb24cd8546de6
MarceloRodriguez <mrg0809@gmail.com>
2021-01-28 21:19:04 -0600

    lo mismo con return

--- main.js
+++ main.js
@@ -3,26 +3,33 @@
 }
 saludar('Hola', 'Pedro');*/
 
-var figura=(prompt('Que figura es:'));
+let figura=(prompt('Que figura es:'));
 
 function cuadradorect(size1, size2, size3, size4) {
-    alert(size1+size2+size3+size4);
+    return(size1+size2+size3+size4);
 }
 
 function circulo(radio){
-    alert(2*3.1416*radio);
+    return(2*3.1416*radio);
+}
+
+function triangulo(size1, size2, size3) {
+    return(size1+size2+size3);
 }
 
 switch (figura){
 
     case 'cuadrado':
-        cuadradorect(4,4,4,4);
+        console.log(cuadradorect(4,4,4,4));
         break;
 
     case 'circulo':
-        circulo(50);
+        console.log(circulo(50));
         break;
 
+    case 'triangulo':
+        console.log(triangulo(9,2,3));
+        break;
 
     default:
         alert("no tengo esa figura");
