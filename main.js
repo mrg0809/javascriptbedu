49e83fc04dc154196ac28bfafce1f58e9465c579
MarceloRodriguez <mrg0809@gmail.com>
2021-02-04 18:56:45 -0600

    lo que no termine

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-var milista = [5, 57, 'Hola', true, 312312];
+/*var milista = [5, 57, 'Hola', true, 312312];
 var indice = Number(prompt('Que indice te muestro?'));
 if (indice>milista.length){
     console.log('Fuera de rango')
     indice = Number(prompt('El indice no existe. Pasame otro:'));
 }
-console.log(milista[indice]);
+console.log(milista[indice]);*/
+
+let valor;
+var arreglo = [];
+
+switch(opcion){
+    case agregar:
+        valor=prompt('Que valor quieres agregar?');
+        arreglo.push(valor);
+        break;
+
+    case borrar:
+        valor=prompt('Que valor deseas borrar?');
+        arreglo.splice(arreglo.find(valor),1);
+        break;
+
+    case imprimir:
+        console.table(arreglo);
+        break;
+
+    case eliminar
+
+}
+
