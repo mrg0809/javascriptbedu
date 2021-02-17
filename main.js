b8ff48c27c59e46c9a9257274ac5fdc2f7a2f150
MarceloRodriguez <mrg0809@gmail.com>
2021-02-16 22:04:59 -0600

    clase8 falta arreglar el queryselectionall

--- main.js
+++ main.js
@@ -1,51 +1,25 @@
 /*
-var numbers = [3, 4, 6, 7, 8, 93, 4];
-var doubles = [];
+var button = document.querySelector("button");
 
-for(var i = 0; i < numbers.length; i++) {
-doubles.push(numbers[i] * 2);
-}
-
-console.log(numbers);
-console.log(doubles);
-*/
-
-// nombre edad y hobby[]
-
-/*let numeros = [3,4,6,7,8,93,4];
-
-const doble = numeros.map((current, index, array) => {
-    if (index % 2){
-        return current * 2
+  button.addEventListener("mousedown", function(event) {
+    if (event.button == 0) {
+      console.log("Boton 1");
+    } else if (event.button == 1) {
+      conole.log("Middle bustton");
+    } else if (event.button == 2) {
+      console.log("Right button");
     }
+  });
+  */
+
+// Falta que funcione en todos los botones
+const boton = document.querySelector(".boton");
+boton.addEventListener("mousedown", () => {
+    console.log(boton.parentNode);
+    const nuevoelemento = document.createElement('h1');
+    nuevoelemento.innerHTML = "Soy NUEVO";
+    document.body.appendChild(nuevoelemento);
 });
-*/
-
-
-//REDUCE EJEMPLO
-const alumnos = [
-    { nombre: 'Maria', calificaciones: [9,9,7,8,10] },
-    { nombre: 'Antonio', calificaciones: [9,8,7,8,10] },
-    { nombre: 'Juan', calificaciones: [9,9,7,8,2] },
-    { nombre: 'Luis', calificaciones: [9,9,7,8] },
-    { nombre: 'Ana', calificaciones: [9,9,7,8,10] },
-    { nombre: 'Maria', calificaciones: [9,9,8,10,5] },
-    { nombre: 'Pedro', calificaciones: [7,9,7,8,10] },
-    { nombre: 'Julio', calificaciones: [6,0,7,8,10] },
-]
-
-var cuadrodehonor = alumnos.reduce((p,c) => {
-    if ((c.calificaciones.reduce((c1,c2) => c1+c2) / 5) > 8) {
-        p = [...p, c] //Esto o push.
-    }
-    return p;
-}, []);
-
-console.log(cuadrodehonor);
-
-
-
-
 
 
 
